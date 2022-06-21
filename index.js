59eeb6bb92ce534cf9fb7e0a77885258eb22fb2d
Linus Norton <linusnorton@gmail.com>
2022-06-21 19:45:52 +0100

    tidy up, add more files

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const branchDir = __dirname + '/' + arg2 + '/';
 
 const fileFieldId = {
   'AuthorisationCaseState.json': field => `${field.CaseStateID}:${field.UserRole}`,
-  'CaseField.json': field => field.ID
+  'AuthorisationCaseType.json': field => field.UserRole,
+  'CaseField.json': field => field.ID,
+  'CaseRoles.json': field => field.ID,
+  'SearchInputFields.json': field => field.CaseFieldID,
+  'SearchResultFields.json': field => field.CaseFieldID,
+  'State.json': field => field.ID,
+  'WorkBasketInputFields.json': field => field.CaseFieldID,
+  'WorkBasketResultFields.json': field => field.CaseFieldID,
+  'AuthorisationCaseEvent/AuthorisationCaseEvent.json': field => `${field.CaseEventID}:${field.UserRole}`
 }
 
 const blacklistedKeys = ['LiveFrom', 'SecurityClassification', 'CaseTypeID'];
@@ -27,6 +35,8 @@ const hasFieldChanged = (obj1, obj2) => Object
   .keys(obj1)
   .some(key => obj1[key] !== obj2[key]);
 
+let output = '';
+
 for (const [file, getFieldId] of Object.entries(fileFieldId)) {
   const masterContent = require(masterDir + file).map(removeBlacklistedKeys);
   const branchContent = require(branchDir + file).map(removeBlacklistedKeys);
@@ -42,6 +52,8 @@ for (const [file, getFieldId] of Object.entries(fileFieldId)) {
     .filter(key => branchFields[key] && hasFieldChanged(masterFields[key], branchFields[key]))
     .map(key => ({ oldValue: masterFields[key], newValue: branchFields[key]}));
 
-  console.log(generateFileReport(file, { additions, changes, removals }));
+  output += generateFileReport(file, { additions, changes, removals });
 }
+
+console.log(output);
 
--- markdown.js
+++ markdown.js
@@ -1,9 +1,14 @@
 
-const getAdditionsOrRemovals = diff => {
+const getAdditionsOrRemovals = (diff, title) => {
+  if (diff.length === 0) {
+    return '';
+  }
+
   const keys = Object.keys(diff[0] || []);
   const rows = diff.map(row => '|' + Object.values(row).join('|')).join('|\n');
 
-  return diff.length === 0 ? '' : `
+  return `
+#### ${diff.length} ${title}
 |${keys.join('|')}|
 |${keys.map(_ => '---').join('|')}|
 ${rows}|
@@ -11,6 +16,10 @@ ${rows}|
 }
 
 const getChanges = diff => {
+  if (diff.length === 0) {
+    return '';
+  }
+
   const keys = Object.keys(diff[0]?.oldValue || []);
   const rows = diff.map(row => keys.map(key => {
     const content = row.oldValue[key] === row.newValue[key]
@@ -20,7 +29,8 @@ const getChanges = diff => {
     return `<td>\n\n${content}\n\n</td>`;
   }).join(''));
 
-  return diff.length === 0 ? '' : `
+  return `
+#### ${diff.length} changed
 <table>
 <thead>
   <tr>
@@ -33,15 +43,14 @@ ${rows.map(row => `<tr>${row}</tr>`).join('')}
 }
 
 const generateFileReport = (file, { additions, removals, changes }) => {
+  if (additions.length + removals.length + changes.length === 0) {
+    return '';
+  }
   return `
 ### ${file}
-#### ${additions.length} added
-${getAdditionsOrRemovals(additions)}  
-#### ${removals.length} removed
-${getAdditionsOrRemovals(removals)}  
-#### ${changes.length} changed
-${getChanges(changes)}
-  `;
+${getAdditionsOrRemovals(additions, 'added')}
+${getAdditionsOrRemovals(removals, 'removed')}
+${getChanges(changes)}`;
 };
 
 module.exports = { generateFileReport };
