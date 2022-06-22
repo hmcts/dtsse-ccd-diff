64991027470d31e084dbf03676709af8e607e316
Linus Norton <linusnorton@gmail.com>
2022-06-22 07:09:22 +0100

    Add glob paths, readme and fix build

--- fs.js
+++ fs.js
@@ -3,8 +3,8 @@ const { glob } = require("glob");
 const getFilename = path => path.substring(path.lastIndexOf('/') + 1);
 
 const resolveFiles = (masterDir, branchDir) => ([file, getFieldId]) => {
-  const masterFiles = glob(masterDir + file, { sync: true });
-  const branchFiles = glob(branchDir + file, { sync: true });
+  const masterFiles = glob.sync(masterDir + file);
+  const branchFiles = glob.sync(branchDir + file);
 
   return masterFiles.map(f => [f, branchFiles.find(bf => getFilename(bf) === getFilename(f)), getFieldId]);
 }
--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const masterDir = __dirname + '/' + arg1 + '/';
 const branchDir = __dirname + '/' + arg2 + '/';
 
 const fileFieldId = {
-  'AuthorisationCaseState.json': field => `${field.CaseStateID}:${field.UserRole}`,
-  'AuthorisationCaseType.json': field => field.UserRole,
-  'CaseField.json': field => field.ID,
-  'CaseRoles.json': field => field.ID,
-  'SearchInputFields.json': field => field.CaseFieldID,
-  'SearchResultFields.json': field => field.CaseFieldID,
+  '**/AuthorisationCaseState.json': field => `${field.CaseStateID}:${field.UserRole}`,
+  '**/AuthorisationCaseType.json': field => field.UserRole,
+  '**/CaseField.json': field => field.ID,
+  '**/CaseRoles.json': field => field.ID,
+  '**/SearchInputFields.json': field => field.CaseFieldID,
+  '**/SearchResultFields.json': field => field.CaseFieldID,
   'State.json': field => field.ID,
-  'WorkBasketInputFields.json': field => field.CaseFieldID,
-  'WorkBasketResultFields.json': field => field.CaseFieldID,
-  'AuthorisationCaseEvent/AuthorisationCaseEvent.json': field => `${field.CaseEventID}:${field.UserRole}`
+  'State/State.json': field => field.ID,
+  '**/WorkBasketInputFields.json': field => field.CaseFieldID,
+  '**/WorkBasketResultFields.json': field => field.CaseFieldID,
+  '**/AuthorisationCaseEvent.json': field => `${field.CaseEventID}:${field.UserRole}`
 }
 
 const output = Object
