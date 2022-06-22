14c26cac8fb1ef02f2f49de2e48947da404fd508
Linus Norton <linusnorton@gmail.com>
2022-06-22 15:33:07 +0100

    account for missing and additional files

--- fs.js
+++ fs.js
@@ -1,16 +1,22 @@
 const { glob } = require('glob');
+const { existsSync } = require('fs');
 
 const [_, __, arg1, arg2] = process.argv;
 const masterDir = process.cwd() + '/' + arg1 + '/';
 const branchDir = process.cwd() + '/' + arg2 + '/';
+
+const loadFile = path => existsSync(path) ? require(path) : [];
 
 const getFilename = path => path.replace(masterDir, '').replace(branchDir, '');
 
 const resolveFiles = ([file, getFieldId]) => {
   const masterFiles = glob.sync(masterDir + file);
   const branchFiles = glob.sync(branchDir + file);
+  const branchOnlyFiles = branchFiles.filter(bf => !masterFiles.some(mf => getFilename(bf) === getFilename(mf)));
+  const allFiles = [...masterFiles, ...branchOnlyFiles].map(getFilename);
 
-  return masterFiles.map(f => [f, branchFiles.find(bf => getFilename(bf) === getFilename(f)), getFieldId]);
+  return allFiles
+    .map(file => [file, loadFile(masterDir + file), loadFile(branchDir + file), getFieldId]);
 };
 
-module.exports = { getFilename, resolveFiles };
+module.exports = { resolveFiles };
